90ef7b2b8eed5900efd8f7bf6279304d589c7127
Lesley Hogg <lesleyhogg@gmail.com>
2019-02-13 11:33:27 -0500

    Updates

---
+++ precache-manifest.6daee8f66fc59ca0edf327f4402f1807.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e3d07953dfb554cfb69",
+    "url": "/movie-app/static/js/runtime~main.6e3d0795.js"
+  },
+  {
+    "revision": "2393dedc4c302216d290",
+    "url": "/movie-app/static/js/main.2393dedc.chunk.js"
+  },
+  {
+    "revision": "5b52eb2f23b2d518997a",
+    "url": "/movie-app/static/js/2.5b52eb2f.chunk.js"
+  },
+  {
+    "revision": "2393dedc4c302216d290",
+    "url": "/movie-app/static/css/main.09c1148c.chunk.css"
+  },
+  {
+    "revision": "769e777752b22dcadfc9e17b1199de85",
+    "url": "/movie-app/index.html"
+  }
+];
